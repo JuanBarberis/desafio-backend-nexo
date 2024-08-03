22821dc7bb73bd7e7095c6182a1d679da2398dfe
JuanCruzBarberis <juanbarberis.97@gmail.com>
2024-08-03 18:29:28 -0300

    ver usuario por dni

---
+++ controllers/users/getById.js
@@ -0,0 +1,38 @@
+import { Address } from "../../models/Address.js";
+import { User } from "../../models/User.js";
+
+const getUserById = async (req, res) => {
+
+    const dni  = req.params.id
+
+    console.log(`Buscando usuario con DNI: ${dni}`)
+    try {
+        const user = await User.findOne({ dni: dni });
+
+        if (!user) {
+            return res.status(404)
+                .json({
+                    success: false,
+                    message: 'Usuario no encontrado'
+                });
+        } else {
+            const addresses = await Address.find({ user_id: user._id });
+           return res.status(200).json({
+                success: true,
+                message: 'Usuario encontrado con exito!',
+                data: {
+                    ...user.toObject(), // Convertir el usuario a un objeto plano
+                    addresses // Agregar las direcciones al objeto del usuario
+                },
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+
+
+export default getUserById;
